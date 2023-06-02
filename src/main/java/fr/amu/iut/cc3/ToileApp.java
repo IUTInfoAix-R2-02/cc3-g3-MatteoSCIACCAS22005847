fde6b79c1075640492324f6439d93034e17acddb
MatteoSCIACCAS22005847 <115480358+MatteoSCIACCAS22005847@users.noreply.github.com>
2023-06-02 14:23:16 +0200

    Commit 2

--- src/main/java/fr/amu/iut/cc3/ToileApp.java
+++ src/main/java/fr/amu/iut/cc3/ToileApp.java
@@ -28,17 +28,17 @@ public class ToileApp extends Application {
         Label labelCpt5 = new Label("Compétence 5 - Conduire ");
         Label labelCpt6 = new Label("Compétence 6 - Collaborer ");
 
-        TextField textCpt1 = new TextField();
-        TextField textCpt2 = new TextField();
-        TextField textCpt3 = new TextField();
-        TextField textCpt4 = new TextField();
-        TextField textCpt5 = new TextField();
-        TextField textCpt6 = new TextField();
+        TextField comp1 = new TextField();
+        TextField comp2 = new TextField();
+        TextField comp3 = new TextField();
+        TextField comp4 = new TextField();
+        TextField comp5 = new TextField();
+        TextField comp6 = new TextField();
 
         Button bouton1 = new Button("Tracer");
         Button bouton2 = new Button("Vider");
 
-        pointsBox.getChildren().addAll(labelCpt1,labelCpt2,labelCpt3,labelCpt4,labelCpt5,labelCpt6,textCpt1,textCpt2,textCpt3,textCpt4,textCpt5,textCpt6,bouton1,bouton2);
+        pointsBox.getChildren().addAll(labelCpt1,labelCpt2,labelCpt3,labelCpt4,labelCpt5,labelCpt6,comp1,comp2,comp3,comp4,comp5,comp6,bouton1,bouton2);
 
         Parent root = FXMLLoader.load(getClass().getResource("toile.fxml"));
         primaryStage.setResizable(false);
